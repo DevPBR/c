bf017209ef8f6dec859d169f3f8861816488ecd9
StarPlatinum_DevPBR <129001970+DevPBR@users.noreply.github.com>
2023-03-26 17:27:52 -0300

    Cambio di anno, anno bisestile
    Il programma controlla se è bisestile e se è febbraio e secondo la possibilità, stampa il calendario

--- Calendario.cpp
+++ Calendario.cpp
@@ -3,53 +3,75 @@
 main()
 {
       int scelta=7, counter, giorni;
-      char feb;
+      char feb, bis;
       do{printf("Che giorno e' il primo del mese?");
       printf("\n0) Lun\n1) Mar\n2) Mer\n3) Gio\n4) Ven\n5) Sab\n6) Dom");
       printf("\nInserire la scelta: ");
       scanf("%d",&scelta);
       printf("\n\n");}while(scelta>6);
       counter=scelta;
-      printf("È febbraio?? 'S' per il sì 'N' per no \n");
+      printf("E febbraio?? 'S' per il si 'N' per no \n");
       getchar();
-      scanf("%c", &feb);
+      scanf("%c", &feb);//ottiene input se è febbraio
+      printf("E un anno Bissestile ? 'S' per si 'N' per no ");
+      getchar();
+      scanf("%c", &bis);//riceve l'iscrizione se è bisestile
       
-      if(feb == 'S')
+      if(feb == 'S' && bis == 'S')//controlla se è febbraio e se è bisestile
       {
       printf("\n\n Lun Mar Mer Gio Ven Sab Dom\n");
-     
                       while(scelta>0){
                                  scelta--;
                                  printf("    ");
                                  }
-
+      for(giorni = 1; giorni < 30; giorni++){
+                 printf("%4d",giorni);
+                 if((giorni+counter)%7==0)printf("\n");
+                 }
+      fflush(stdin);
+      }
+      else if(feb == 'S' && bis == 'N')//controlla se è febbraio e se è bisestile
+      {
+            printf("\n\n Lun Mar Mer Gio Ven Sab Dom\n");
+                      while(scelta>0){
+                                 scelta--;
+                                 printf("    ");
+                                 }
       for(giorni = 1; giorni < 29; giorni++){
                  printf("%4d",giorni);
                  if((giorni+counter)%7==0)printf("\n");
                  }
     
       fflush(stdin);
-
       }
-      else if(feb == 'N')
+      else if(feb == 'N' && bis == 'N')//controlla se è febbraio e se è bisestile
       {
-            printf("\n\n Lun Mar Mer Gio Ven Sab Dom\n");
+           printf("\n\n Lun Mar Mer Gio Ven Sab Dom\n");
      
                       while(scelta>0){
                                  scelta--;
                                  printf("    ");
                                  }
-
       for(giorni = 1; giorni < 32; giorni++){
                  printf("%4d",giorni);
                  if((giorni+counter)%7==0)printf("\n");
                  }
-    
-      fflush(stdin);
+      }
+      else if(feb == 'N' && bis == 'S') //controlla se è febbraio e se è bisestile
+      {
+            printf("\n\n Lun Mar Mer Gio Ven Sab Dom\n");
+                      while(scelta>0){
+                                 scelta--;
+                                 printf("    ");
+                                 }
+      for(giorni = 1; giorni < 32; giorni++){
+                 printf("%4d",giorni);
+                 if((giorni+counter)%7==0)printf("\n");
+                 }
       }
       else
       {
-            printf("Failed");
+            printf("Failed"); //Qualsiasi inserimento errato, comporterà un fallimento
       }
-      return(0);
+      return(0);  
 }
